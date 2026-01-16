34767b8557d9a9955aa4624fcbb0fcd287f98b6e
ozythings <54993448+ozythings@users.noreply.github.com>
2026-01-16 03:18:54 +0300

    refactor(ecomm): change on db

---
+++ scripts/init.sql
@@ -0,0 +1,69 @@
+-- sqlite foreign key thingy
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    gender TEXT,
+    city TEXT,
+    signup_date TIMESTAMP
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    product_id TEXT PRIMARY KEY,
+    product_name TEXT,
+    category TEXT,
+    brand TEXT,
+    price REAL,
+    rating REAL
+);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+    order_id TEXT PRIMARY KEY,
+    user_id TEXT,
+    order_date TIMESTAMP,
+    order_status TEXT,
+    total_amount REAL,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS order_items;
+CREATE TABLE order_items (
+    order_item_id TEXT PRIMARY KEY,
+    order_id TEXT,
+    product_id TEXT,
+    quantity INTEGER,
+    item_price REAL,
+    item_total REAL, -- Bunu şimdilik tutuyoruz (sadece user_id siliyoruz)
+    FOREIGN KEY(order_id) REFERENCES orders(order_id),
+    FOREIGN KEY(product_id) REFERENCES products(product_id)
+);
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events (
+    event_id TEXT PRIMARY KEY,
+    user_id TEXT,
+    product_id TEXT,
+    event_type TEXT,
+    event_timestamp TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(user_id),
+    FOREIGN KEY(product_id) REFERENCES products(product_id)
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+    review_id TEXT PRIMARY KEY,
+    order_id TEXT,
+    product_id TEXT,
+    user_id TEXT,
+    rating INTEGER,
+    review_text TEXT,
+    review_date TIMESTAMP,
+    FOREIGN KEY(order_id) REFERENCES orders(order_id),
+    FOREIGN KEY(product_id) REFERENCES products(product_id),
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
